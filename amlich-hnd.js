39a3d92947e2c486157a5c2a3a8f9fb4e3529aa3
Bùi Khương Duy <okngayvn@gmail.com>
2016-03-23 16:30:56 +0700

    Update amlich-hnd.js

--- amlich-hnd.js
+++ amlich-hnd.js
@@ -495,24 +495,24 @@ function getPrevMonthLink(mm, yy) {
 	var mm1 = mm > 1 ? mm-1 : 12;
 	var yy1 = mm > 1 ? yy : yy-1;
 	//return '<a href="'+window.location.pathname+'?yy='+yy1+'&mm='+mm1+'"><img src="left1.gif" width=8 height=12 alt="PrevMonth" border=0></a>';
-	return '<a href="'+window.location.pathname+'?yy='+yy1+'&mm='+mm1+'">Sau</a>';
+	return '<a href="'+window.location.pathname+'?yy='+yy1+'&mm='+mm1+'">[+]</a>';
 }
 
 function getNextMonthLink(mm, yy) {
 	var mm1 = mm < 12 ? mm+1 : 1;
 	var yy1 = mm < 12 ? yy : yy+1;
 	//return '<a href="'+window.location.pathname+'?yy='+yy1+'&mm='+mm1+'"><img src="right1.gif" width=8 height=12 alt="NextMonth" border=0></a>';
-	return '<a href="'+window.location.pathname+'?yy='+yy1+'&mm='+mm1+'">Trước</a>';
+	return '<a href="'+window.location.pathname+'?yy='+yy1+'&mm='+mm1+'">[-]</a>';
 }
 
 function getPrevYearLink(mm, yy) {
 	//return '<a href="'+window.location.pathname+'?yy='+(yy-1)+'&mm='+mm+'"><img src="left2.gif" width=16 height=12 alt="PrevYear" border=0></a>';
-	return '<a href="'+window.location.pathname+'?yy='+(yy-1)+'&mm='+mm+'"><<</a>';
+	return '<a href="'+window.location.pathname+'?yy='+(yy-1)+'&mm='+mm+'"><<<</a>';
 }
 
 function getNextYearLink(mm, yy) {
 	//return '<a href="'+window.location.pathname+'?yy='+(yy+1)+'&mm='+mm+'"><img src="right2.gif" width=16 height=12 alt="NextYear" border=0></a>';
-	return '<a href="'+window.location.pathname+'?yy='+(yy+1)+'&mm='+mm+'"> >></a>';
+	return '<a href="'+window.location.pathname+'?yy='+(yy+1)+'&mm='+mm+'"> >>></a>';
 }
 
 function printHead(mm, yy) {
